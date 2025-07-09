75a85d18cff98b7dfd260a3fa0cc989722f9e089
beto franca <beto2franca@gmail.com>
2025-07-09 20:22:06 -0300

    parte 1 formulario ok

--- src/app/calculadora/calculadora.ts
+++ src/app/calculadora/calculadora.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculadora.html',
   styleUrl: './calculadora.scss'
 })
