cdc1471fc2920e275853387fc848305c0d59468b
beto franca <beto2franca@gmail.com>
2025-07-16 19:47:09 -0300

    finalizando lista compra

--- src/app/lista-compras/lista-compras.ts
+++ src/app/lista-compras/lista-compras.ts
@@ -24,6 +24,13 @@ export class ListaCompras {
         this.item = '';
 
     }
+
+    riscarItem(itemlista: ItemLista){
+      itemlista.comprado = !itemlista.comprado;
+    }
+    limparLista(){
+      this.lista = [];
+    }
 }
 
 
